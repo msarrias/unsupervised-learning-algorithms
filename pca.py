714ebe423274466f301dee601e8e60e037b0d880
Marina <marina.sarrias@gmail.com>
2020-11-02 22:25:59 +0100

    more experiments with pca

--- pca.py
+++ pca.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numpy.linalg as linalg
+from matplotlib import pyplot as plt
+import matplotlib.ticker as mticker
 
 class pca:   
     def __init__(self, M):
@@ -17,6 +19,24 @@ class pca:
         images_standardized = numerator/ ((images.std(axis=0)+eps).reshape(1, -1) )
         return images_standardized
     
+    def plot_eigenspectrum(self):
+        x = list(range(len(self.eigenValues)))
+        y = self.eigenValues
+        plt.plot(x, y,c="blue")
+        f = mticker.ScalarFormatter(useOffset=False,
+                                    useMathText=True)
+        g = lambda x, pos : "${}$".format(
+            f._formatSciNotation('%1.10e' % x))
+        plt.gca().yaxis.set_major_formatter(
+            mticker.FuncFormatter(g))
+        plt.xlim([0,len(self.eigenValues)])
+        plt.ylim([0, max(self.eigenValues)])
+        plt.xlabel ('i')
+        plt.ylabel (r"$\lambda_{i}$")
+        plt.title("Eigenvalue spectrum")
+        plt.show()
+
+    
     def sort_by_eigenv(self, eigenVal, eigenVect):
         idx = eigenVal.argsort()[::-1]   
         eigenVal = eigenVal[idx]
@@ -32,8 +52,8 @@ class pca:
         return self.sort_by_eigenv(eigenValues, eigenVectors)
     
     def fit_pca(self, standardized_images):
-        cov_matrix = self.compute_covariance_matrix(standardized_images)
-        self.eigenValues, self.eigenVectors = self.spectral_decomposition(cov_matrix)
+        self.cov_matrix = self.compute_covariance_matrix(standardized_images)
+        self.eigenValues, self.eigenVectors = self.spectral_decomposition(self.cov_matrix)
         self.new_coordinates = np.dot(self.eigenVectors[:,0: self.M].T,
                                       standardized_images.T)       
         
